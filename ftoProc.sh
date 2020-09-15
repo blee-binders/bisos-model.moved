3e02422b23977c2b6ec050a82459ca7ff5593658
Mohsen Banan <git@mohsen.1.banan.byname.net>
2020-09-15 13:19:26 -0700

    auto

--- ftoProc.sh
+++ ftoProc.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-####+BEGIN: bx:dblock:bash:top-of-file :vc "cvs" partof: "bystar" :copyleft "halaal+minimal"
+####+BEGIN: bx:bash:top-of-file :vc "cvs" partof: "bystar" :copyleft "halaal+minimal"
+### Args: :control "enabled|disabled|hide|release|fVar"  :vc "cvs|git|nil" :partof "bystar|nil" :copyleft "halaal+minimal|halaal+brief|nil"
+typeset RcsId="$Id: dblock-iim-bash.el,v 1.4 2017-02-08 06:42:32 lsipusr Exp $"
+# *CopyLeft*
+__copying__="
+* Libre-Halaal Software"
+#  This is part of ByStar Libre Services. http://www.by-star.net
+#  This is a Halaal Poly-Existential. See http://www.freeprotocols.org
 
 ####+END:
 
-####+BEGIN: bx:dblock:lsip:bash:seed-spec :types "seedFtoCommon.sh"
+####+BEGIN: bx:bsip:bash:seed-spec :types "seedFtoCommon.sh"
 SEED="
-*  /[dblock]/ /Seed/ :: [[file:/opt/public/osmt/bin/seedFtoCommon.sh]] | 
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/panels/bisos/model/ftoProc.sh 
+*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/bisos-model/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
-    /opt/public/osmt/bin/seedFtoCommon.sh -l $0 "$@" 
+    /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
     exit $?
 fi
 ####+END:
@@ -34,13 +41,14 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:leavesList :types ""
 # {{{ DBLOCK-leavesList
 leavesList="
-modelAndTerminology
-fileTreeObject-FTO
-bisosAccts
+universalBisos
 fileParameters
 bisosProvisioning
-ICM
+bisosAccts
 fileVariables
+ICM
+fileTreeObject-FTO
+modelAndTerminology
 "
 # }}} DBLOCK-leavesList
 ####+END:
