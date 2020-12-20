4c14457f273063117723267107805f4107c0ff19
Mohsen Banan <git@mohsen.1.banan.byname.net>
2020-12-20 10:01:47 -0800

    dblock updates.

--- universalBisos/ftoProc.sh
+++ universalBisos/ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/bisos-model/universalBisos/ftoProc.sh 
+*  /This File/ :: /bisos/panels/bisos/model/universalBisos/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
